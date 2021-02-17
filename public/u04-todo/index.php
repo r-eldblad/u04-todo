46652372ebb3d949d69d34df67c8294a6d138362
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-02-17 12:17:12 +0100

    added index.php db.php, as well as  added a form that will be used in the todo app

---
+++ public/u04-todo/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TODO-App</title>
+</head>
+<body>
+    <form action="" method="POST">
+        <label for="name">Title</label>
+        <input id="name" type="text" name="name" value="Enter title">
+        <label for="name">Task</label>
+        <input id="task" type="text" name="name" value="Enter task">
+        <button type="submit" name="add">Add &#43;</button>
+    </form>
+</body>
+</html>
