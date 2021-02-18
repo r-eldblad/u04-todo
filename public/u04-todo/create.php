1c4a060028ff87f51161349dbd4c6b1d3f3de70b
Rasmus Eldblad <r.eldblad@protonmail.com>
2021-02-18 10:41:29 +0100

    insertion of data inside database now works

--- public/u04-todo/create.php
+++ public/u04-todo/create.php
@@ -2,16 +2,14 @@
 
 require_once(__DIR__ . '/db/database.php');
 
-if (isset($_POST['add'])) {
-
-    $dat
+if (isset($_POST['submit'])) {
 
     $title = $_POST['title'];
     $task = $_POST['task'];
 
-    $sql = "INSERT INTO tasks (title, task_message, done) VALUES('$title', '$task')";
+    $sql = "INSERT INTO tasks (title, task_message) VALUES(:title, :task)";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute([$title, $task]);
+    $stmt->execute(['title' => $title, 'task' => $task]);
 
 };
 
--- public/u04-todo/index.php
+++ public/u04-todo/index.php
@@ -6,15 +6,20 @@
     <title>TODO-App</title>
 </head>
 <body>
-<?php require_once(__DIR__ . '/create.php'); ?>
-<?php require(__DIR__ . '/db/database.php'); ?>
+
+<?php 
+
+require_once(__DIR__ . '/create.php');
+require_once(__DIR__ . '/db/database.php');
+
+?>
 
     <form action="" method="POST">
         <label for="title">Title</label>
         <input id="title" type="text" name="title" placeholder="Enter title">
-        <label for="name">Task</label>
+        <label for="task">Task</label>
         <input id="task" type="text" name="task" placeholder="Enter task">
-        <button type="submit" name="add">Add &#43;</button>
+        <button type="submit" name="submit">Add &#43;</button>
     </form>
 </body>
 </html>
